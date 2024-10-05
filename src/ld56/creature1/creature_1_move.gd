94f0a85a6d9e78dc71bc1ecacd837ca15d0398e1
Nicolas Charette-Naud <nicolas@bamboosoft.ca>
2024-10-05 16:21:08 -0400

    Look at and move fowards the player

--- src/ld56/creature1/creature_1_move.gd
+++ src/ld56/creature1/creature_1_move.gd
@@ -1,4 +1,6 @@
-extends Node3D
+class_name CreatureMove extends Node3D
+
+@export var base_detection_radius: float = 3.0
 
 @export var path_points: Array[Node3D] = [];
 var path_points_distance: Array[float] = [];
@@ -7,24 +9,45 @@ var path_index = 0
 var path_count = 0
 var t = 0.0
 
+var player: Player
+
+var player_in_range: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	path_count = len(path_points)
+	player = get_node("../Player")
 	
+	path_count = len(path_points)
 	for i in range(0, path_count):
 		var nextIndex = _next_index(i)
 		var diff = path_points[i].position - path_points[nextIndex].position
 		path_points_distance.append(diff.length())
 	
-	print(len(path_points_distance))
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	_check_player_distance()
+	if player_in_range:
+		_move_to_player(delta)
+		_look_at_player()
+	else:
+		_move_on_path(delta)
+	
+func _move_to_player(delta: float):
+	var playerAtLevel = Vector3(player.position.x, position.y, player.position.z)
+	var direction = playerAtLevel - position
+	var movement = direction.normalized() * delta
+	position += movement
+	
+func _look_at_player():
+	var direction = player.position - position
+	var look_pos = position - direction
+	look_at(look_pos)
+	
+func _move_on_path(delta: float):
 	if(len(path_points_distance) == 0):
 		return
 	
 	t += delta
-	
 	var currentDistanceToDo = path_points_distance[path_index]
 	
 	var nextIndex = _next_index(path_index)
@@ -39,6 +62,18 @@ func _process(delta: float) -> void:
 	if(weight >= 1):
 		t -= currentDistanceToDo
 		path_index = _next_index(path_index)
+	
+func _check_player_distance():
+	var playerAtLevel = Vector3(player.position.x, position.y, player.position.z)
+	var distance = (playerAtLevel - position).length()
+	
+	player_in_range = distance < base_detection_radius
+	
+	var distance_text = ""
+	if player_in_range:
+		distance_text = "InRange"
+	
+	StatsScreen.distance(var_to_str(distance) + distance_text)
 	
 func _next_index(index: int) -> int:
 	var nextIndex = index + 1
